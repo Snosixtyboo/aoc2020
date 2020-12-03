d1211f1ffc3ed5e24e2eb48f6a34cda49126998b
Bernhard Kerbl <kerbl@icg.tugraz.at>
2020-12-03 16:15:15 +0100

    Uploaded first 3

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+version1 = False
+
 inputfile = open('data/in1.txt', 'r')
 lines = inputfile.readlines()
 
@@ -7,8 +9,13 @@ for line in lines:
 
 for i in range(len(numbers)):
     for j in range(i+1, len(numbers)):
-        for k in range(j+1, len(numbers)):
-            if numbers[i] + numbers[j] + numbers[k] == 2020:
-                print(numbers[i]*numbers[j]*numbers[k])
+        if version1:
+            if numbers[i] + numbers[j] == 2020:
+                print(numbers[i]*numbers[j])
                 break
+        else:
+            for k in range(j+1, len(numbers)):
+                if numbers[i] + numbers[j] + numbers[k] == 2020:
+                    print(numbers[i]*numbers[j]*numbers[k])
+                    break
         
