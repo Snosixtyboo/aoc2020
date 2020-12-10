17d1d8c2ed146132a7a39bd5af8aea4464b64725
Bernhard Kerbl <kerbl@icg.tugraz.at>
2020-12-10 12:34:16 +0100

    Day 10 done

---
+++ 10.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var version2 bool
+	flag.BoolVar(&version2, "v2", false, "Use solution for second task")
+	flag.Parse()
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	lines := strings.Split(string(bytes), "\n")
+	nums, possibleWays := make([]int64, len(lines)+1), make([]int64, len(lines)+2)
+	for i, line := range lines {
+		nums[i+1], _ = strconv.ParseInt(line, 0, 64)
+	}
+	sort.Slice(nums, func(i, j int) bool { return nums[i] < nums[j] })
+	nums = append(nums, nums[len(nums)-1]+3)
+	possibleWays[0] = 1
+
+	differences := [4]int{}
+	for i := 0; i < len(nums)-1; i++ {
+		differences[nums[i+1]-nums[i]]++
+		for n := i + 1; n < len(nums) && nums[n]-nums[i] <= 3; n++ {
+			possibleWays[n] += possibleWays[i]
+		}
+	}
+	if !version2 {
+		fmt.Printf("Solution: %d\n", differences[1]*differences[3])
+	} else {
+		fmt.Printf("Solution: %d\n", possibleWays[len(possibleWays)-1])
+	}
+}
